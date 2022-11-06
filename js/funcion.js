53854450e37222df05ef4d7654a0477f27bd9e51
AgusFW <agustina.witis@gmail.com>
2022-11-06 16:28:31 -0300

    ultima version

--- js/funcion.js
+++ js/funcion.js
@@ -34,24 +34,24 @@ document.getElementById("total").value=result;
 
 function calcular()
 {
-    const ticket = 200;
-    var cantidad=document.getElementById("cantidad").value;
+    ticket=200;
+    cantidad=document.getElementById("cantidad").value;
 
-    var categoria=document.getElementById("categoria");
-    var valorCat=categoria.options(categoria.categoriaIndex).value;
+    var select=document.getElementById("categoria");
+    var valorSel=select.options[select.selectedIndex].value;
 
     result=0;
 
-    switch(valorCat)
+    switch(valorSel=parseInt(valorSel))
     {
         case 1:
-            result= cantidad*ticket - ((cantidad*ticket)*valorCat)
+            result= cantidad*ticket - ((cantidad*ticket)*0.8)
             break;
         case 2:
-            result= cantidad*ticket - ((cantidad*ticket)*valorCat)
+            result= cantidad*ticket - ((cantidad*ticket)*0.5)
             break;
         case 3:
-            result= cantidad*ticket - ((cantidad*ticket)*valorCat)
+            result= cantidad*ticket - ((cantidad*ticket)*0.15)
             break;
         case 4:
             result= cantidad*ticket
